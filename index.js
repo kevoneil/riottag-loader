ad8e21af3299c482f3a7982a7abb2215b9b71076
Kevin O'Neil <kevinmoneil@me.com>
2016-11-29 14:38:36 +0900

    adds loader-utils package, start testing loader

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 var compiler = require('riot-compiler');
+var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
 
+  var opts = loaderUtils.getLoaderConfig(this, "riottag-loader");
+
   this.cacheable();
 
-  return compiler.compile(source);
+  try {
+    return compiler.compile(source, opts, this.resourcePath);
+  } catch (error) {
+    throw new Error(error);
+  }
 
 }
