a0c7118f74a8eb81cf597029dee68d7e3db9893e
Kevin O'Neil <kevinmoneil@me.com>
2016-12-27 20:23:35 +0900

    not passing

--- test/index.js
+++ test/index.js
@@ -7,22 +7,21 @@ const webpack = require('../index.js')
 
 describe('riottag-loader', function() {
 
-  const expectedDir =  path.join(__dirname, 'compiled')
+  const compiledDir =  path.join(__dirname, 'compiled')
   const tagDir =  path.join(__dirname, 'tag')
 
   function normalize(str) {
-    const string = str.trim()
-    console.log(str);
-    // return str.trim().replace(/[/\\n\r]+/g, '')
+    return str.trim().replace(/[/\\n\r]+/g, '')
   }
 
+  //NOTE: figure out why it won't trim
   function getFile(name) {
-    return fsp.readFile(path.join(expectedDir, name), 'utf8')
-      .then(res => normalize(res))
+    return fsp.readFile(path.join(compiledDir, name), 'utf8')
+      .then(res => console.log(res))
   }
 
   function tagFiles(name) {
-    return fsp.readFile(path.join(tagDir, name), 'utf-8')
+    return fsp.readFile(path.join(tagDir, name), {encoding:'utf8'})
       .then(res => webpack(normalize(res)))
   }
 
